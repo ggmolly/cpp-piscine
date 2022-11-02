138ba3afa416475ab0d91f59b3d1ed73e5976e4b
Jonas ALLERHAND <jallerha@student.42.fr>
2022-11-02 16:06:50 +0100

    fix an infinite loop if 'from' is empty

--- 01/ex04/replace.cpp
+++ 01/ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 12:28:14 by jallerha          #+#    #+#             */
-/*   Updated: 2022/11/02 13:56:40 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/02 15:44:45 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	str_replace(std::string &haystack, std::string &from, std::string &to)
 {
 	if (from == to)
+		return ;
+	if (from.length() == 0)
 		return ;
 	while (true)
 	{
