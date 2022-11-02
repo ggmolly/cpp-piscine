4b1746d3755f51ae23804a08c25331b68693437c
Jonas ALLERHAND <jallerha@student.42.fr>
2022-11-02 16:06:36 +0100

    check size of horde

--- 01/ex01/zombieHorde.cpp
+++ 01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 21:32:46 by jallerha          #+#    #+#             */
-/*   Updated: 2022/10/31 21:37:37 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/02 15:41:28 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
+	if (N <= 0)
+	{
+		std::cout << "Invalid N for horde.";
+		return (NULL);
+	}
 	Zombie *zombies = new Zombie[N];
 	for (int i = 0; i < N; i++)
 		zombies[i].setName(name);
