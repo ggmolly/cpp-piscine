a1a7ea5f8239d642b2526cdccda3ef4881fc80d3
Jonas ALLERHAND <jallerha@student.42.fr>
2022-11-04 11:55:44 +0100

    space tests in output

--- 01/ex03/main.cpp
+++ 01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 22:15:58 by jallerha          #+#    #+#             */
-/*   Updated: 2022/11/01 23:15:58 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/04 11:54:56 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int main()
 {
+	std::cout << std::endl << ">>> TEST 1 <<<" << std::endl << std::endl;
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
@@ -24,6 +25,7 @@ int main()
 		club.setType("some other type of club");
 		bob.attack();
 	}
+	std::cout << std::endl << ">>> TEST 2 <<<" << std::endl << std::endl;
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
