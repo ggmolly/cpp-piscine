7e079a1f49c757866649cb73da8ede895c0d7462
Jonas ALLERHAND <jallerha@student.42.fr>
2022-11-13 14:52:35 +0100

    [cpp04 -- ex00] fixed tests

--- 04/ex00/srcs/main.cpp
+++ 04/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:58:16 by jallerha          #+#    #+#             */
-/*   Updated: 2022/11/09 23:09:20 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/13 14:51:35 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,93 +14,54 @@
 #include "Dog.hpp"
 #include "Animal.hpp"
 #include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
-/* -------- Stack tests -------- */
-void animalStack()
-{
-	std::cout << ">>> (stack) Default Animal <<<" << std::endl;
-	Animal defaultAnimal;
-	std::cout << "Type : " << defaultAnimal.getType() << std::endl;
-	defaultAnimal.makeSound();
-}
-
-void catStack()
-{
-	std::cout << "\n>>> (stack) Cat <<<" << std::endl;
-	Cat cat;
-	std::cout << "Type : " << cat.getType() << std::endl;
-	cat.makeSound();
-}
-
-void dogStack()
-{
-	std::cout << "\n>>> (stack) Dog <<<" << std::endl;
-	Dog dog;
-	std::cout << "Type : " << dog.getType() << std::endl;
-	dog.makeSound();
-}
-
-/* -------- Heap tests -------- */
-
-void animalHeap()
-{
-	std::cout << ">>> (heap) Default Animal <<<" << std::endl;
-	Animal *defaultAnimal = new Animal();
-	std::cout << "Type : " << defaultAnimal->getType() << std::endl;
-	defaultAnimal->makeSound();
-	delete defaultAnimal;
-}
-
-void catHeap()
-{
-	std::cout << "\n>>> (heap) Cat <<<" << std::endl;
-	Cat *cat = new Cat();
-	std::cout << "Type : " << cat->getType() << std::endl;
-	cat->makeSound();
-	delete cat;
-}
-
-void dogHeap()
-{
-	std::cout << "\n>>> (heap) Dog <<<" << std::endl;
-	Dog *dog = new Dog();
-	std::cout << "Type : " << dog->getType() << std::endl;
-	dog->makeSound();
-	delete dog;
-}
-
-void heapTests()
-{
-	animalHeap();
-	catHeap();
-	dogHeap();
-	std::cout << std::endl;
-}
-
-void stackTests()
-{
-	animalStack();
-	catStack();
-	dogStack();
-	std::cout << std::endl;
-}
-
-/* -------- WrongAnimal tests -------- */
-
-void wrongTests()
-{
-	std::cout << ">>> WrongCat <<<" << std::endl;
-	WrongAnimal *wrongCat = new WrongAnimal();
-	std::cout << "Type : " << wrongCat->getType() << std::endl;
-	wrongCat->makeSound();
-	delete wrongCat;
-}
-
-/* -------- Main -------- */
+# define PLUM1 "\033[38;5;219m"
+# define PLUM2 "\033[38;5;183m"
+# define PLUM3 "\033[38;5;176m"
+# define PLUM4 "\033[38;5;96m"
+# define SPRINGGREEN1 "\033[38;5;48m"
+# define RESET "\033[0m"
+# define RED1 "\033[38;5;196m"
 
 int main(void)
 {
-	stackTests();
-	heapTests();
-	wrongTests();
+	{
+		const Animal *generic = new Animal();
+		const Animal *dog = new Dog();
+		const Animal *cat = new Cat();
+
+		std::cout << PLUM1 << "Generic animal says: " << SPRINGGREEN1;
+		generic->makeSound();
+		std::cout << PLUM2 << "Dog says: " << SPRINGGREEN1;
+		dog->makeSound();
+		std::cout << PLUM3 << "Cat says: " << SPRINGGREEN1;
+		cat->makeSound();
+		std::cout << RED1 << "-------------------------" << RESET << std::endl;
+		delete generic;
+		delete dog;
+		delete cat;
+	}
+	std::cout << std::endl;
+	{
+		const WrongAnimal *wrong = new WrongAnimal();
+		const WrongAnimal *wrongCat = new WrongCat();
+
+		std::cout << PLUM4 << "Wrong animal says: " << SPRINGGREEN1;
+		wrong->makeSound();
+		std::cout << PLUM1 << "WrongCat says: " << SPRINGGREEN1;
+		wrongCat->makeSound();
+		std::cout << RED1 << "-------------------------" << RESET << std::endl;
+		delete wrong;
+		delete wrongCat;
+	}
+	std::cout << std::endl;
+	{
+		const WrongCat *wrongCat = new WrongCat();
+
+		std::cout << PLUM1 << "WrongCat says: " << SPRINGGREEN1;
+		wrongCat->makeSound();
+		std::cout << RED1 << "-------------------------" << RESET << std::endl;
+		delete wrongCat;
+	}
 }
