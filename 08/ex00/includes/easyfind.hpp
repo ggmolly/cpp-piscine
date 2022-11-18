c63338c14634894d2a45807865ff776e6b71e0af
Jonas ALLERHAND <jallerha@student.42.fr>
2022-11-18 14:37:37 +0100

    [cpp08 -- ex00] minor changes in header

--- 08/ex00/includes/easyfind.hpp
+++ 08/ex00/includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 01:29:27 by jallerha          #+#    #+#             */
-/*   Updated: 2022/11/18 01:30:03 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/18 14:18:22 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 #include <algorithm>
 
 template <typename T>
-int easyfind(T &container, int n)
+typename T::iterator easyfind(T &container, int n)
 {
 	typename T::iterator it;
 
 	it = std::find(container.begin(), container.end(), n);
 	if (it == container.end())
-		throw std::exception();
-	return (*it);
+		throw std::overflow_error("Unable to find n in passed container");
+	return it;
 }
